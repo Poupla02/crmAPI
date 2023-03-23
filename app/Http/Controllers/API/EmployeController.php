5d23645ec78760a6194f4f59deb40edfc039e824
poupla02 <pouplasome@gmail.com>
2023-03-23 12:06:46 +0000

    create employe and entreprise

---
+++ app/Http/Controllers/API/EmployeController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreEmployeRequest;
+use App\Http\Requests\UpdateEmployeRequest;
+use App\Models\Employe;
+
+class EmployeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEmployeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Employe $employe)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEmployeRequest $request, Employe $employe)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Employe $employe)
+    {
+        //
+    }
+}
